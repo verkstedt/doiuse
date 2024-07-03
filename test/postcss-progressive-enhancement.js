a87783ae40a0161847d1798d1d952d9901a18b7f
Marek Augustynowicz <marek.augustynowicz@verkstedt.com>
2024-07-03 15:22:38 +0200

    Allow progressive enhancement using @supports

--- test/postcss-progressive-enhancement.js
+++ test/postcss-progressive-enhancement.js
@@ -20,3 +20,51 @@ test('Progressive enhancement using repeated CSS properties', (t) => {
 
   t.end();
 });
+
+test('Progressive enhancement using @supports', (t) => {
+  const css = `
+    p {
+      display: block;
+    }
+
+    @supports (display: grid) {
+      p {
+        display: grid;
+        flex-wrap: wrap;
+      }
+    }
+  `;
+
+  const result = postcss(new DoIUse({
+    browsers: ['safari >= 9'],
+  })).process(css);
+  const warnings = result.warnings();
+
+  t.equal(warnings.length, 0, 'No warnings');
+
+  t.end();
+});
+
+test('Progressive enhancement using @supports with multiple conditions', (t) => {
+  const css = `
+    p {
+      display: block;
+    }
+
+    @supports (display: flex) and (color: hsl(0, 0%, 0%)) {
+      p {
+        display: flex;
+        background-color: hsl(0, 0%, 0%);
+      }
+    }
+  `;
+
+  const result = postcss(new DoIUse({
+    browsers: ['safari >= 9'],
+  })).process(css);
+  const warnings = result.warnings();
+
+  t.equal(warnings.length, 0, 'No warnings');
+
+  t.end();
+});
